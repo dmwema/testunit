d8b1bd4f1246f7f976abf2691c65bdd699711057
dmwema <danielmwemakapwe@gmail.com>
2023-01-31 00:02:23 +0100

    add fixtures

---
+++ tests/AppTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Tests;
+use Monolog\Test\TestCase;
+
+class AppTest extends TestCase
+{
+    public function testTestAreWorking()
+    {
+        $this->assertEquals(2, 1 + 1);
+    }
+}
